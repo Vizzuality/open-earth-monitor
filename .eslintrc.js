1adfe265900f904e8c0853dad2daa0ceb4ab0b54
mluena <maluenarod@gmail.com>
2023-08-29 19:25:19 +0200

    ui components set up

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@
 module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:prettier/recommended',
     'next/core-web-vitals',
   ],
