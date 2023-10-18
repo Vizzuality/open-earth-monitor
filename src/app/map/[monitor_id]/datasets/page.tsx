4118590b2c1d6645cf8440a00882a5a6b6ad2507
David Inga <david.inga@vizzuality.com>
2023-10-18 10:00:56 +0200

    implementing swipe control on the map based on ol-ext library

--- src/app/map/[monitor_id]/datasets/page.tsx
+++ src/app/map/[monitor_id]/datasets/page.tsx
@@ -1,14 +1,21 @@
-'use client';
+import { notFound } from 'next/navigation';
 
-import type { NextPage } from 'next';
+import type { Metadata, NextPage } from 'next';
 
 import { useMonitorLayers } from '@/hooks/monitors';
 
 import DatasetCard from '@/components/datasets/card';
 import Loading from '@/components/loading';
 
+export const metadata: Metadata = {
+  title: 'Hub - Open Earth Monitor Cyberinfrastructure',
+  description: '...',
+};
+
 const DatasetsPage: NextPage<{ params: { monitor_id: string } }> = ({ params: { monitor_id } }) => {
   const { data, isLoading, isFetched, isError } = useMonitorLayers({ monitor_id });
+
+  if (!isError) return notFound();
 
   return (
     <div>
